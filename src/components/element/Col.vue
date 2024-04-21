10152ebb2bf942e76d761fec77a216d6fac2d34e
daytoywhy <1320007006@qq.com>
2024-04-21 22:34:29 +0800

    row col组件编写

---
+++ src/components/element/Col.vue
@@ -0,0 +1,232 @@
+<template>
+  <div :class="['el-col', ...classList]" :style="style">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ElCol',
+  props: {
+    span: {
+      type: Number,
+      default: 24
+    },
+    offset: {
+      type: Number,
+      default: 0
+    },
+    push: {
+      type: Number,
+      default: 0
+    },
+    pull: {
+      type: Number,
+      default: 0
+    },
+    xs: [Number, Object],
+    sm: [Number, Object],
+    md: [Number, Object],
+    lg: [Number, Object],
+    xl: [Number, Object]
+  },
+  computed: {
+    gutter () {
+      let parent = this.$parent
+      while (parent && parent.$options.name !== 'ElRow') {
+        parent = parent.$parent
+      }
+      return parent ? parent.gutter : 0
+    },
+    style () {
+      return {
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px'
+      }
+    },
+    classList () {
+      let classList = [];
+
+      ['span', 'offset', 'push', 'pull'].forEach(prop => {
+        if (this[prop]) {
+          classList.push(
+            prop === 'span' ? `el-col-${this[prop]}` : `el-col-${prop}-${this[prop]}`
+          )
+        }
+      });
+      ['xs', 'sm', 'md', 'lg', 'xl'].forEach(size => {
+        if (typeof this[size] === 'number') {
+          classList.push(`el-col-${size}-${this[size]}`)
+        } else if (typeof this[size] === 'object') {
+          let props = this[size]
+          Object.keys(props).forEach(prop => {
+            classList.push(
+              prop !== 'span' ? `el-col-${size}-${prop}-${props[prop]}` : `el-col-${size}-${props[prop]}`
+            )
+          })
+        }
+      })
+      return classList
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.el-col {
+  float: left;
+  box-sizing: border-box;
+}
+
+
+
+@for $i from 0 through 24 {
+  .el-col-#{$i} {
+    width: calc($i / 24) * 100%;
+  }
+
+  .el-col-offset-#{$i} {
+    margin-left: calc($i / 24) * 100%;
+  }
+
+  .el-col-push-#{$i} {
+    position: relative;
+    left: calc($i / 24) * 100%;
+  }
+
+  .el-col-pull-#{$i} {
+    position: relative;
+    right: calc($i / 24) * 100%;
+  }
+}
+
+@media only screen and (max-width: calc(768px - 1px)) {
+  @for $i from 0 through 24 {
+    .el-col-xs-0 {
+      display: none;
+    }
+
+    .el-col-xs-#{$i} {
+      width: calc($i / 24) * 100%;
+    }
+
+    .el-col-xs-offset-#{$i} {
+      margin-left: calc($i / 24) * 100%;
+    }
+
+    .el-col-xs-push-#{$i} {
+      position: relative;
+      left: calc($i / 24) * 100%;
+    }
+
+    .el-col-xs-pull-#{$i} {
+      position: relative;
+      right: calc($i / 24) * 100%;
+    }
+  }
+}
+
+@media only screen and (min-width: 768px) {
+  @for $i from 0 through 24 {
+    .el-col-sm-0 {
+      display: none;
+    }
+
+    .el-col-sm-#{$i} {
+      width: calc($i / 24) * 100%;
+    }
+
+    .el-col-sm-offset-#{$i} {
+      margin-left: calc($i / 24) * 100%;
+    }
+
+    .el-col-sm-push-#{$i} {
+      position: relative;
+      left: calc($i / 24) * 100%;
+    }
+
+    .el-col-sm-pull-#{$i} {
+      position: relative;
+      right: calc($i / 24) * 100%;
+    }
+  }
+}
+
+@media only screen and (min-width: 992px) {
+  @for $i from 0 through 24 {
+    .el-col-md-0 {
+      display: none;
+    }
+
+    .el-col-md-#{$i} {
+      width: calc($i / 24) * 100%;
+    }
+
+    .el-col-md-offset-#{$i} {
+      margin-left: calc($i / 24) * 100%;
+    }
+
+    .el-col-md-push-#{$i} {
+      position: relative;
+      left: calc($i / 24) * 100%;
+    }
+
+    .el-col-md-pull-#{$i} {
+      position: relative;
+      right: calc($i / 24) * 100%;
+    }
+  }
+}
+
+@media only screen and (min-width: 1200px) {
+  @for $i from 0 through 24 {
+    .el-col-lg-0 {
+      display: none;
+    }
+
+    .el-col-lg-#{$i} {
+      width: calc($i / 24) * 100%;
+    }
+
+    .el-col-lg-offset-#{$i} {
+      margin-left: calc($i / 24) * 100%;
+    }
+
+    .el-col-lg-push-#{$i} {
+      position: relative;
+      left: calc($i / 24) * 100%;
+    }
+
+    .el-col-lg-pull-#{$i} {
+      position: relative;
+      right: calc($i / 24) * 100%;
+    }
+  }
+}
+
+@media only screen and (min-width: 1920px) {
+  @for $i from 0 through 24 {
+    .el-col-xl-0 {
+      display: none;
+    }
+
+    .el-col-xl-#{$i} {
+      width: calc($i / 24) * 100%;
+    }
+
+    .el-col-xl-offset-#{$i} {
+      margin-left: calc($i / 24) * 100%;
+    }
+
+    .el-col-xl-push-#{$i} {
+      position: relative;
+      left: calc($i / 24) * 100%;
+    }
+
+    .el-col-xl-pull-#{$i} {
+      position: relative;
+      right: calc($i / 24) * 100%;
+    }
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@/assets/theme-chalk/display.scss'
+createApp(App).mount('#app')
